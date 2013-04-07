618901164395b026b72de3e60bfa185bdd3b8c4d
mikolaj <gishelher@gmail.com>
2013-04-07 22:35:39 +0200

    fixes #8 resources providing example in bundle viewService

--- viewService/src/main/scala/istuff/view/service/impl/IndexViewResource.scala
+++ viewService/src/main/scala/istuff/view/service/impl/IndexViewResource.scala
@@ -4,13 +4,16 @@ import javax.ws.rs._
 import core.MediaType
 import istuff.widget.service.api.WidgetService
 
-@Path("")
+@Path("/view")
 class IndexViewResource(widgetServiceRef: (((WidgetService) => Any) => Option[Any])) {
 
   @GET @Produces(Array(MediaType.TEXT_HTML))
   def index : String = {
-    "<html><head><title>iStuff</title></head><body>" +
-    (widgetServiceRef { _.getAvailableWidgets } match {
+    "<html><head>" +
+      "<title>iStuff</title>" +
+      "</head><body>" +
+      "<script src=\"js/simple.js\"></script>" +
+      (widgetServiceRef { _.getAvailableWidgets } match {
         case None => "No widgets available"
         case Some(widgets:Seq[String]) => widgets.mkString
     }) +
--- viewService/src/main/scala/istuff/view/service/internal/Activator.scala
+++ viewService/src/main/scala/istuff/view/service/internal/Activator.scala
@@ -4,16 +4,26 @@ import org.osgi.framework._
 import com.weiglewilczek.scalamodules._
 import istuff.view.service.impl.IndexViewResource
 import istuff.widget.service.api.WidgetService
+import org.osgi.service.http.HttpService
 
 class Activator extends BundleActivator {
   var serviceRegistration: ServiceRegistration = _
+  var httpService : ServiceFinder[HttpService] = null
 
   def start(context: BundleContext) {
     val widgetServiceRef:((WidgetService => Any) => Option[Any]) = context findService withInterface[WidgetService] andApply _
     val bundle = new IndexViewResource(widgetServiceRef)
     serviceRegistration = context.createService(bundle)
+
+    httpService = context findService withInterface[HttpService]
+    httpService andApply { _.registerResources("/","/htmls",null) } match {
+      case None => println("res fail")
+      case _ => println("res success")
+    }
+
   }
 
   def stop(context: BundleContext) {
+    httpService andUnget
   }
 }
