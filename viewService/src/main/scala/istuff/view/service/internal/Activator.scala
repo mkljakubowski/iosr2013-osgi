48554ac7bea015e0b6142630f55e1c071c0f8b85
Tomasz Godzik <tomek.godzik@gmail.com>
2013-04-18 18:14:16 +0200

    Added creating a html response from file.

--- viewService/src/main/scala/istuff/view/service/internal/Activator.scala
+++ viewService/src/main/scala/istuff/view/service/internal/Activator.scala
@@ -7,17 +7,12 @@ import istuff.widget.service.api.WidgetService
 import org.osgi.service.http.HttpService
 
 import org.amdatu.template.processor.{TemplateProcessor, TemplateContext, TemplateEngine}
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
+import java.io.File
 
+class HTMLGenerateServlet(context:BundleContext) extends HttpServlet{
 
-class Activator extends BundleActivator {
-  var serviceRegistration: ServiceRegistration = _
-  var httpService : ServiceFinder[HttpService] = null
-
-  def start(context: BundleContext) {
-    val widgetServiceRef:((WidgetService => Any) => Option[Any]) = context findService withInterface[WidgetService] andApply _
-    val bundle = new IndexViewResource(widgetServiceRef)
-    serviceRegistration = context.createService(bundle)
-
+  override def doGet(req : HttpServletRequest , resp : HttpServletResponse ) {
     // Retrieve a Velocity implementation of the engine
     val eng = context findService classOf[TemplateEngine]
 
@@ -30,10 +25,8 @@ class Activator extends BundleActivator {
 
     var processor : TemplateProcessor  = null
 
-    eng andApply { _.createProcessor(
-      "<html><body><h1>${title}</h1>" +
-        "<p>${text}</p>" +
-        "</body></html>")
+    val file = new File("viewService/src/main/resources/htmls/index.html")
+    eng andApply { _.createProcessor(file)
     }   match {
       case None => println("No key with that name!")
       case Some(x) =>   processor=x
@@ -42,14 +35,29 @@ class Activator extends BundleActivator {
     tcontext.put("title", "My First Document")
     tcontext.put("text", "This is a short story about a story that was generated with a template engine.")
 
-    System.out.println(processor.generateString(tcontext))
+    resp.setContentType("text/html;charset=UTF-8")
+    resp.setContentType("text/html")
+    val out = resp.getWriter()
+    out.print(processor.generateString(tcontext))
+  }
+}
+class Activator extends BundleActivator {
+  var serviceRegistration: ServiceRegistration = _
+  var httpService : ServiceFinder[HttpService] = null
+
+  def start(context: BundleContext) {
+    val widgetServiceRef:((WidgetService => Any) => Option[Any]) = context findService withInterface[WidgetService] andApply _
+    val bundle = new IndexViewResource(widgetServiceRef)
+    serviceRegistration = context.createService(bundle)
 
 
-//    httpService = context findService withInterface[HttpService]
-//    httpService andApply { _.registerServlet("/hell",new HelloWorld(),null,null) } match {
-//      case None => println("res fail")
-//      case _ => println("res success")
-//    }
+
+
+   httpService = context findService withInterface[HttpService]
+    httpService andApply { _.registerServlet("/hell",new HTMLGenerateServlet(context),null,null) } match {
+      case None => println("res fail")
+      case _ => println("res success")
+    }
 
     httpService andApply { _.registerResources("/","/htmls",null) } match {
       case None => println("res fail")
