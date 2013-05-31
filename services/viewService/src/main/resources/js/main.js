493bd1fde02b18449e0be86f790d8dded6feb6bb
Tomasz Godzik <tomek.godzik@gmail.com>
2013-05-31 17:35:18 +0200

    Now we need to click to edit.

--- services/viewService/src/main/resources/js/main.js
+++ services/viewService/src/main/resources/js/main.js
@@ -1,4 +1,3 @@
-
 //$(function() {
 //    $("#resizable").resizable();
 //
@@ -9,8 +8,37 @@
 //    });
 //});
 
-function logout(){
-    $.post('/login', function(data) {
+function logout() {
+    $.post('/login', function (data) {
         window.location.href = "/";
     });
+}
+
+
+var isDisabled = true
+var first = true
+function changeResize() {
+
+    if (!isDisabled) {                      //make disabled
+        $(".resizable").resizable("disable");
+        ;
+        $(".resizable").draggable("disable");
+        $(".edit_button").text("Edit widgets")
+        $('div.ui-resizable-handle').hide();
+        isDisabled = true
+    }
+    else {                      //make enabled
+        if (first) {
+            $(".resizable").resizable();
+            $(".resizable").draggable();
+            first=false
+        }
+        else {
+            $(".resizable").resizable("enable");
+            $(".resizable").draggable("enable");
+            $('div.ui-resizable-handle').show()
+        }
+        $(".edit_button").text("Stop editing")
+        isDisabled = false
+    }
 }
