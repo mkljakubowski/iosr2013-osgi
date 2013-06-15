18744af39d1845488013c6e9232b71471020acff
Piotr Borowiec <ppborowiec@gmail.com>
2013-06-15 13:46:25 +0200

    userService tests

--- services/userService/src/tests/istuff/user/service/tests/CustomizerViewTests.scala
+++ services/userService/src/tests/istuff/user/service/tests/CustomizerViewTests.scala
@@ -1,12 +1,16 @@
 package istuff.user.service.tests
 
+import istuff.user.service.impl.{WidgetsView, CustomizerView}
 import org.scalatest.FlatSpec
-import com.mongodb.{DBCollection, DB}
+import com.mongodb.{DBCursor, DBCollection}
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import org.scalatest.mock.MockitoSugar
 import org.osgi.framework.BundleContext
-import istuff.database.service.impl.DatabaseImpl
+import javax.servlet.http.{HttpSession, HttpServletResponse, HttpServletRequest}
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import matchers.IsAnyDBObject
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,12 +22,73 @@ import istuff.database.service.impl.DatabaseImpl
 @RunWith(classOf[JUnitRunner])
 class CustomizerViewTests extends FlatSpec with MockitoSugar{
 
-  "CustomerView" should "supply injected DB" in {
+  "CustomizerView" should "update user widget collection" in {
     // Arrange
+    val name = "johnny"
+    val context = mock[BundleContext]
+    val userWidgetColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+    val session = mock[HttpSession]
+
+    stub(req.getSession(any[Boolean])).toReturn(session)
+    stub(session.getAttribute("user")).toReturn(name)
+    stub(session.getAttribute("login")).toReturn("authenticated")
+
+    val view = new CustomizerView(context, userWidgetColl)
 
     // Act
+    view.doPost(req, resp)
 
     // Assert
+    verify(userWidgetColl).remove(argThat(new IsAnyDBObject()))
+    verify(userWidgetColl).insert(argThat(new IsAnyDBObject()))
+  }
+
+  it should "redirect to login page if session is not authenticated" in {
+    // Arrange
+    val name = "johnny"
+
+    val context = mock[BundleContext]
+    val userColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+    val session = mock[HttpSession]
+
+    stub(req.getSession(any[Boolean])).toReturn(session)
+    stub(session.getAttribute("user")).toReturn(name)
+    stub(session.getAttribute("login")).toReturn("not_authenticated")
+
+    val view = new CustomizerView(context, userColl)
+
+    // Act
+    view.doPost(req, resp)
+
+    // Assert
+    verify(resp).sendRedirect("/login")
+  }
+
+  it should "redirect to login page if session is null" in {
+    // Arrange
+    val name = "johnny"
+
+    val context = mock[BundleContext]
+    val userColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+    val session = mock[HttpSession]
+
+    stub(req.getSession(any[Boolean])).toReturn(null)
+    stub(session.getAttribute("user")).toReturn(name)
+    stub(session.getAttribute("login")).toReturn("authenticated")
+
+    val view = new CustomizerView(context, userColl)
+
+    // Act
+    view.doPost(req, resp)
+
+    // Assert
+    verify(resp).sendRedirect("/login")
   }
 
 }
--- services/userService/src/tests/istuff/user/service/tests/RegisterViewTests.scala
+++ services/userService/src/tests/istuff/user/service/tests/RegisterViewTests.scala
@@ -1,10 +1,16 @@
 package istuff.user.service.tests
 
+import _root_.istuff.user.service.impl.RegisterView
 import org.scalatest.FlatSpec
-import com.mongodb.DB
+import com.mongodb.{DBCursor, DBCollection}
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
 import org.scalatest.mock.MockitoSugar
+import org.osgi.framework.BundleContext
+import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
+import org.mockito.Mockito._
+import org.mockito.Matchers.argThat
+import matchers.{IsAnyDBObject, IsAnyString}
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,4 +22,102 @@ import org.scalatest.mock.MockitoSugar
 @RunWith(classOf[JUnitRunner])
 class RegisterViewTests extends FlatSpec with MockitoSugar{
 
+  "RegisterView" should "not register user with void username" in {
+    // Arrange
+    val name = "johnny1"
+    val pwd = "JohnSmith"
+
+    val context = mock[BundleContext]
+    val userColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+
+    stub(req.getParameter("user")).toReturn(name)
+    stub(req.getParameter("pwd1")).toReturn(pwd)
+
+    val view = new RegisterView(context, userColl)
+
+    // Act
+    view.doPost(req, resp)
+
+    // Assert
+    verify(resp, never()).setHeader(argThat(new IsAnyString()),argThat(new IsAnyString()))
+  }
+
+  it should "not register user with two different passwords" in {
+    // Arrange
+    val name = "johnny1"
+    val pwd = "JohnSmith"
+
+    val context = mock[BundleContext]
+    val userColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+
+    stub(req.getParameter("user")).toReturn(name)
+    stub(req.getParameter("pwd1")).toReturn(pwd)
+    stub(req.getParameter("pwd2")).toReturn(pwd+"different password")
+
+    val view = new RegisterView(context, userColl)
+
+    // Act
+    view.doPost(req, resp)
+
+    // Assert
+    verify(resp, never()).setHeader(argThat(new IsAnyString()),argThat(new IsAnyString()))
+  }
+
+  it should "not register user with username which is already taken" in {
+    // Arrange
+    val name = "johnny1"
+    val pwd = "JohnSmith"
+
+    val context = mock[BundleContext]
+    val userColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+    val cursor = mock[DBCursor]
+
+    stub(req.getParameter("user")).toReturn(name)
+    stub(req.getParameter("pwd1")).toReturn(pwd)
+    stub(req.getParameter("pwd2")).toReturn(pwd)
+    stub(userColl.find(argThat(new IsAnyDBObject))).toReturn(cursor)
+    stub(cursor.count).toReturn(1)
+
+    val view = new RegisterView(context, userColl)
+
+    // Act
+    view.doPost(req, resp)
+
+    // Assert
+    verify(resp).setHeader("redirect_to", "/register")
+    verify(userColl, never()).insert(argThat(new IsAnyDBObject))
+  }
+
+  it should "register user with unique username and matching passwords and redirect to login page" in {
+    // Arrange
+    val name = "johnny1"
+    val pwd = "JohnSmith"
+
+    val context = mock[BundleContext]
+    val userColl = mock[DBCollection]
+    val req = mock[HttpServletRequest]
+    val resp = mock[HttpServletResponse]
+    val cursor = mock[DBCursor]
+
+    stub(req.getParameter("user")).toReturn(name)
+    stub(req.getParameter("pwd1")).toReturn(pwd)
+    stub(req.getParameter("pwd2")).toReturn(pwd)
+    stub(userColl.find(argThat(new IsAnyDBObject))).toReturn(cursor)
+    stub(cursor.count).toReturn(0)
+
+    val view = new RegisterView(context, userColl)
+
+    // Act
+    view.doPost(req, resp)
+
+    // Assert
+    verify(resp).setHeader("redirect_to", "/login")
+    verify(userColl).insert(argThat(new IsAnyDBObject))
+  }
 }
